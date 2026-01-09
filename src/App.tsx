a965ac1d16589fa2e0140699c0bcc1927faaa0e8
cloudgal42 <alternativeego666@gmail.com>
2026-01-09 22:04:01 +0700

    remove unused sections

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ const App = () => {
     "  }"
     const city = JSON.parse(cityJson);
 
+  // TODO: Migrate all regular bootstrap classes with react-bootstrap
   return (
     <>
       <div className="container-lg mt-3">
@@ -68,33 +69,13 @@ const App = () => {
           <div>
             <h2>Cities</h2>
           </div>
-          <div id="city-feed" className="d-flex gap-2">
+          <div id="city-feed" className="row gx-2 gy-2">
             <CityCard city={city} />
             <CityCard city={city} />
             <CityCard city={city} />
           </div>
         </section>
       </div>
-      {/*<div>*/}
-      {/*  <a href="https://vite.dev" target="_blank">*/}
-      {/*    <img src={viteLogo} className="logo" alt="Vite logo" />*/}
-      {/*  </a>*/}
-      {/*  <a href="https://react.dev" target="_blank">*/}
-      {/*    <img src={reactLogo} className="logo react" alt="React logo" />*/}
-      {/*  </a>*/}
-      {/*</div>*/}
-      {/*<h1>Vite + React</h1>*/}
-      {/*<div className="card">*/}
-      {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
-      {/*    count is {count}*/}
-      {/*  </button>*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to test HMR*/}
-      {/*  </p>*/}
-      {/*</div>*/}
-      {/*<p className="read-the-docs">*/}
-      {/*  Click on the Vite and React logos to learn more*/}
-      {/*</p>*/}
     </>
   )
 }
