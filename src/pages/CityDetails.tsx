631b71095af1a3c6f66319fd4267498c50f025da
cloudgal42 <alternativeego666@gmail.com>
2026-01-12 16:14:56 +0700

    implement WIP city details page

--- src/pages/CityDetails.tsx
+++ src/pages/CityDetails.tsx
@@ -1,7 +1,46 @@
-export const CityDetails = () => {
-  return (
-    <div className="main-wrapper flex-grow-1 ms-sm-5 me-sm-5">
+import {Card} from "react-bootstrap";
+import {useOutletContext, useParams} from "react-router";
+import type {ContextType} from "../App.tsx";
+import {Cake2, Person} from "react-bootstrap-icons";
 
-    </div>
-  )
+export const CityDetails = () => {
+  const params = useParams();
+
+  const {
+    cities,
+  } = useOutletContext<ContextType>();
+
+  const city = cities.find(city => city.cityName === params.city);
+
+  if (!city) {
+    return (
+      <div>City not found</div>
+    )
+  } else {
+    return (
+      <div className="main-wrapper flex-grow-1 ms-sm-5 me-sm-5">
+        <h2>{city.cityName}</h2>
+        <div className="mt-3"></div>
+        <div className="mt-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>City Stats</Card.Title>
+              <ul className="list-unstyled d-flex flex-row flex-wrap justify-content-between gap-2">
+                <li className="d-flex align-items-center gap-2">
+                  <Person/>
+                  {city.cityPopulation.toLocaleString()}
+                </li>
+                <li className="d-flex align-items-center gap-2">
+                  <Cake2 />
+                  {city.createdAtFormatted}
+                </li>
+              </ul>
+              <Card.Title>HoF Stats</Card.Title>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    )
+  }
 }
+
