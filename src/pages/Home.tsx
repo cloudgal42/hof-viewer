675ff3e27b1a2067c2e56cb72a03ef44f8e66cf2
cloudgal42 <alternativeego666@gmail.com>
2026-01-13 09:11:27 +0700

    fix favoriting percentage not rounded to nearest integer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -66,7 +66,7 @@ function groupCities(citiesToGroup: City[]) {
     });
 
     if (screenshotStat.combinedStats) {
-      screenshotStat.combinedStats.favoritingPercentage = screenshotStat.combinedStats.uniqueViewsCount / screenshotStat.combinedStats.favoritesCount;
+      screenshotStat.combinedStats.favoritingPercentage = Math.round(screenshotStat.combinedStats.uniqueViewsCount / screenshotStat.combinedStats.favoritesCount);
         groupedCities.push(screenshotStat.combinedStats);
     }
   });
