1dc273ffb2f98b5b9300419c7cfbbd26eb407e48
cloudgal42 <alternativeego666@gmail.com>
2026-02-24 10:48:21 +0700

    refactor: extract details section to separate component

--- src/pages/CityDetails.tsx
+++ src/pages/CityDetails.tsx
@@ -1,51 +1,24 @@
-import {Button, Card, OverlayTrigger, Tooltip} from "react-bootstrap";
-import {NavLink, useNavigate, useOutletContext, useParams, useSearchParams} from "react-router";
+import {Button} from "react-bootstrap";
+import {NavLink, useOutletContext, useParams, useSearchParams} from "react-router";
 import type {ContextType} from "../App.tsx";
-import {BoxArrowUpRight, ChevronDown, Eye, Heart, Person, Trophy} from "react-bootstrap-icons";
+import {ChevronDown} from "react-bootstrap-icons";
 import {lazy, Suspense, useState} from "react";
 import {DEFAULT_IMAGES_PER_PAGE} from "../components/details/CityGallery/CityGallery.tsx";
 
 import PlaceholderImg from "../assets/placeholder.svg"
-import {ModList} from "../components/details/Playset/ModList.tsx";
-import {RenderSettings} from "../components/details/RenderSettings/RenderSettings.tsx";
 import {PlaceholderDetails} from "../components/details/Details/PlaceholderDetails.tsx";
 import {CityTrends} from "../components/details/CityTrends/CityTrends.tsx";
 import {ErrorScreen} from "../components/misc/ErrorScreen/ErrorScreen.tsx";
 import {useQuery} from "@tanstack/react-query";
 import type {City} from "../interfaces/City.ts";
-import {PlaceholderFeatModCard} from "../components/details/FeatModCard/PlaceholderFeatModCard.tsx";
-import {FeatModCard} from "../components/details/FeatModCard/FeatModCard.tsx";
-import * as React from "react";
 import {CityInsights} from "../components/details/CityInsights/CityInsights.tsx";
 import {useCreatorCities} from "../hooks/useCreatorCities.ts";
 import {groupCities} from "../utils/GroupCities.ts";
 
 import {AdaptiveHeader} from "../components/details/AdaptiveHeader/AdaptiveHeader.tsx";
+import {Details} from "../components/details/Details/Details.tsx";
 
 const CityGallery = lazy(() => import("../components/details/CityGallery/CityGallery.tsx"));
-
-const cityMilestones = [
-  "Tiny Village",
-  "Small Village",
-  "Large Village",
-  "Grand Village",
-  "Tiny Town",
-  "Boom Town",
-  "Busy Town",
-  "Big Town",
-  "Great Town",
-  "Small City",
-  "Big City",
-  "Large City",
-  "Huge City",
-  "Grand City",
-  "Metropolis",
-  "Thriving Metropolis",
-  "Flourishing Metropolis",
-  "Expansive Metropolis",
-  "Massive Metropolis",
-  "Megalopolis",
-]
 
 const CityDetails = () => {
   const {
@@ -168,92 +141,7 @@ const CityDetails = () => {
           id="details"
           className={`mt-3 position-relative ${(isLoadMoreHovered && !isLastPage) && "load-more-hovered"}`}
         >
-          <Card>
-            <Card.Body>
-              {cityDetails.showcasedModId && (
-                <section className="mb-3">
-                  <h3>
-                    <Card.Title>Showcased Asset/Map</Card.Title>
-                  </h3>
-                  {isFetching && !cityDetails.showcasedMod ? (
-                    <PlaceholderFeatModCard/>
-                  ) : (
-                    <FeatModCard fetchError={error} showcasedMod={cityDetails.showcasedMod}/>
-                  )}
-                  {/*<a href={`https://mods.paradoxplaza.com/mods/${city.showcasedModId}/Windows`} target="_blank">*/}
-                  {/*  {city.showcasedModId}*/}
-                  {/*</a>*/}
-                </section>
-              )}
-              <section className="mb-3">
-                <h3>
-                  <Card.Title>Stats</Card.Title>
-                </h3>
-                <OverlayTrigger overlay={<Tooltip>{cityDetails.createdAtFormattedDistance}</Tooltip>}>
-                  <p className="d-inline-block text-muted mb-1">First posted
-                    on: {new Date(cityDetails.createdAt).toLocaleString()}</p>
-                </OverlayTrigger>
-                <ul className="list-unstyled mb-0 row">
-                  <li className="col-sm-6 col-md-4 col-lg-3 d-flex align-items-center gap-2">
-                    <Person/>
-                    <span className="visually-hidden">Population</span>
-                    {cityDetails.cityPopulation.toLocaleString()}
-                  </li>
-                  <li className="col-sm-6 col-md-4 col-lg-3 d-flex align-items-center gap-2">
-                    <Trophy/>
-                    <span className="visually-hidden">Milestone</span>
-                    {cityMilestones[cityDetails.cityMilestone - 1]}
-                  </li>
-                  <li className="col-sm-6 col-md-4 col-lg-3 d-flex align-items-center gap-2">
-                    <Eye/>
-                    <span className="visually-hidden">Unique Views</span>
-                    {`${cityDetails.viewsCount.toLocaleString()} (Unique: ${cityDetails.uniqueViewsCount.toLocaleString()})`}
-                  </li>
-                  <li className="col-sm-6 col-md-4 col-lg-3 d-flex align-items-center gap-2">
-                    <Heart/>
-                    <span className="visually-hidden">Favorites</span>
-                    {`${cityDetails.favoritesCount.toLocaleString()} (${cityDetails.favoritingPercentage}% of unique views)`}
-                  </li>
-                </ul>
-              </section>
-              <section className="mb-3">
-                <h3>
-                  <Card.Title>Map Used</Card.Title>
-                </h3>
-                {cityDetails.mapName ? (
-                  <p>
-                    <span>{cityDetails.mapName} (</span>
-                    <a
-                      target="_blank"
-                      className="d-inline-flex align-items-center gap-2"
-                      href={`https://mods.paradoxplaza.com/games/cities_skylines_2?search=${cityDetails.mapName}`}
-                    >
-                      Search on PDX Mods
-                      <BoxArrowUpRight width="16" height="16"/>
-                    </a>
-                    )
-                  </p>
-                ) : (
-                  <p>This screenshot was posted before map sharing was possible.</p>
-                )}
-              </section>
-              <section className="mb-3">
-                <h3>
-                  <Card.Title>Playset</Card.Title>
-                </h3>
-                <ModList city={cityDetails}/>
-              </section>
-              {/* Avoid displaying render settings for grouped cities */}
-              {!Array.isArray(cityDetails?.imageUrlFHD) && (
-                <section>
-                  <h3>
-                    <Card.Title>Render Settings</Card.Title>
-                  </h3>
-                  <RenderSettings city={cityDetails}/>
-                </section>
-              )}
-            </Card.Body>
-          </Card>
+          <Details cityDetails={cityDetails} error={error} isFetching={isFetching} />
         </section>
         <section
           id="trends"
