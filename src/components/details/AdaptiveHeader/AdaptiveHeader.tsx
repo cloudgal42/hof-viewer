567ae2c1276b460fd51586881313a03da1fd2d58
cloudgal42 <alternativeego666@gmail.com>
2026-02-26 18:29:35 +0700

    refactor: AdaptiveHeader to be more reusable by other pages

--- src/components/details/AdaptiveHeader/AdaptiveHeader.tsx
+++ src/components/details/AdaptiveHeader/AdaptiveHeader.tsx
@@ -1,17 +1,13 @@
-import {BackButton} from "../../misc/BackButton/BackButton.tsx";
-import {CreatorPreviewTrigger} from "../../misc/CreatorPreview/CreatorPreviewTrigger.tsx";
-import type {City, GroupedCities} from "../../../interfaces/City.ts";
-import {useEffect, useState} from "react";
+import {type ReactNode, useEffect, useState} from "react";
 
 import "../../../css/components/AdaptiveHeader.scss";
 
 export const AdaptiveHeader = (
-  {cityDetails} : {cityDetails: City | GroupedCities}
+  {children} : {children: ReactNode}
 ) => {
   const [headerCollapsed, setHeaderCollapsed] = useState(false);
 
   useEffect(() => {
-
     function checkScrollYPos() {
       if (window.scrollY >= 70) {
         setHeaderCollapsed(true);
@@ -28,18 +24,7 @@ export const AdaptiveHeader = (
   return (
     <div className={headerCollapsed ? " header-collapsed" : "adaptive-header"}>
       <div className="header-collapsed-body main-wrapper m-auto">
-        <div className="h2-container d-flex align-items-center">
-          <BackButton />
-          <h2 className="mb-0">
-            {cityDetails.cityName}{cityDetails.cityNameTranslated && `(${cityDetails.cityNameTranslated})`}
-          </h2>
-        </div>
-        <CreatorPreviewTrigger
-          creator={cityDetails.creator.creatorName}
-          showLinks={true}
-        >
-          <h3 className="text-muted d-inline">by {cityDetails.creator.creatorName}</h3>
-        </CreatorPreviewTrigger>
+        {children}
       </div>
     </div>
   )
