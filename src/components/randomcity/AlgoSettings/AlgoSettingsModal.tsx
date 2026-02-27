87af594c6ffe13987388b4cb9a336d53b6032b64
cloudgal42 <alternativeego666@gmail.com>
2026-02-27 16:22:23 +0700

    refactor: extract range settings to separate component

--- src/components/randomcity/AlgoSettings/AlgoSettingsModal.tsx
+++ src/components/randomcity/AlgoSettings/AlgoSettingsModal.tsx
@@ -2,6 +2,7 @@ import {type Dispatch, type SetStateAction, useState} from "react";
 import {Button, Form, Modal, OverlayTrigger, Tooltip} from "react-bootstrap";
 import type {RandomAlgoSettings} from "../../../interfaces/RandomAlgoSettings.ts";
 import {ArrowClockwise, InfoCircle} from "react-bootstrap-icons";
+import {RangeSetting} from "./RangeSetting.tsx";
 
 const DEFAULT_SETTINGS = {
   random: 5,
@@ -97,222 +98,76 @@ export const AlgoSettingsModal = (
           should be picked next.
         </p>
         <Form>
-          <div className="mb-2">
-            <Form.Label htmlFor="randomVal" className="d-flex align-items-center gap-2">
-              Random
-              <OverlayTrigger overlay={<Tooltip>Screenshots chosen completely by random.</Tooltip>}>
-                <InfoCircle/>
-              </OverlayTrigger>
-              {randomVal !== randomAlgoSettings.random && (
-                <OverlayTrigger overlay={<Tooltip>Reset to last saved value</Tooltip>}>
-                  <button
-                    className="ms-2 p-0 border-0 bg-transparent d-flex align-items-center"
-                    onClick={() => setRandomVal(randomAlgoSettings.random)}
-                  >
-                    <span className="visually-hidden">Reset to last saved value</span>
-                    <ArrowClockwise/>
-                  </button>
-                </OverlayTrigger>
-              )}
-            </Form.Label>
-            <div className="d-flex align-items-center gap-4">
-              <Form.Range
-                id="randomVal"
-                aria-label="randomValCounter"
-                min="0"
-                max="10"
-                value={randomVal}
-                onChange={e => setRandomVal(parseInt(e.currentTarget.value))}
-              />
-              <span id="randomValCounter">{randomVal}</span>
-            </div>
-          </div>
-          <div className="mb-2">
-            <Form.Label htmlFor="popularVal" className="d-flex align-items-center gap-2">
-              Popular
-              <OverlayTrigger overlay={<Tooltip>Screenshots that have an above average likes/views ratio.</Tooltip>}>
-                <InfoCircle/>
-              </OverlayTrigger>
-              {popularVal !== randomAlgoSettings.popular && (
-                <OverlayTrigger overlay={<Tooltip>Reset to last saved value</Tooltip>}>
-                  <button
-                    className="ms-2 p-0 border-0 bg-transparent d-flex align-items-center"
-                    onClick={() => setPopularVal(randomAlgoSettings.popular)}
-                  >
-                    <span className="visually-hidden">Reset to last saved value</span>
-                    <ArrowClockwise/>
-                  </button>
-                </OverlayTrigger>
-              )}
-            </Form.Label>
-            <div className="d-flex align-items-center gap-4">
-              <Form.Range
-                id="popularVal"
-                aria-describedby="popularValCounter"
-                min="0"
-                max="10"
-                value={popularVal}
-                onChange={e => setPopularVal(parseInt(e.currentTarget.value))}
-              />
-              <span id="popularValCounter">{popularVal}</span>
-            </div>
-          </div>
-          <div className="mb-2">
-            <Form.Label htmlFor="trendingVal" className="d-flex align-items-center gap-2">
-              Trending
-              <OverlayTrigger
-                overlay={<Tooltip>Popular screenshots that have the highest likes/views ratio, regardless of
-                  age.</Tooltip>}>
-                <InfoCircle/>
-              </OverlayTrigger>
-              {trendingVal !== randomAlgoSettings.trending && (
-                <OverlayTrigger overlay={<Tooltip>Reset to last saved value</Tooltip>}>
-                  <button
-                    className="ms-2 p-0 border-0 bg-transparent d-flex align-items-center"
-                    onClick={() => setTrendingVal(randomAlgoSettings.trending)}
-                  >
-                    <span className="visually-hidden">Reset to last saved value</span>
-                    <ArrowClockwise/>
-                  </button>
-                </OverlayTrigger>
-              )}
-            </Form.Label>
-            <div className="d-flex align-items-center gap-4">
-              <Form.Range
-                id="trendingVal"
-                aria-describedby="trendingValCounter"
-                min="0"
-                max="10"
-                value={trendingVal}
-                onChange={e => setTrendingVal(parseInt(e.currentTarget.value))}
-              />
-              <span id="trendingValCounter">{trendingVal}</span>
-            </div>
-          </div>
-          <div className="mb-2">
-            <Form.Label htmlFor="recentVal" className="d-flex align-items-center gap-2">
-              Recent
-              <OverlayTrigger overlay={<Tooltip>Recently posted screenshots (up to a few days old)</Tooltip>}>
-                <InfoCircle/>
-              </OverlayTrigger>
-              {recentVal !== randomAlgoSettings.recent && (
-                <OverlayTrigger overlay={<Tooltip>Reset to last saved value</Tooltip>}>
-                  <button
-                    className="ms-2 p-0 border-0 bg-transparent d-flex align-items-center"
-                    onClick={() => setRecentVal(randomAlgoSettings.recent)}
-                  >
-                    <span className="visually-hidden">Reset to last saved value</span>
-                    <ArrowClockwise/>
-                  </button>
-                </OverlayTrigger>
-              )}
-            </Form.Label>
-            <div className="d-flex align-items-center gap-4">
-              <Form.Range
-                id="recentVal"
-                aria-describedby="recentValCounter"
-                min="0"
-                max="10"
-                value={recentVal}
-                onChange={e => setRecentVal(parseInt(e.currentTarget.value))}
-              />
-              <span id="recentValCounter">{recentVal}</span>
-            </div>
-          </div>
-          <div className="mb-2">
-            <Form.Label htmlFor="archeologistVal" className="d-flex align-items-center gap-2">
-              Forgotten Ones
-              <OverlayTrigger overlay={<Tooltip>Screenshots that have not seen any activity for some time.</Tooltip>}>
-                <InfoCircle/>
-              </OverlayTrigger>
-              {archeologistVal !== randomAlgoSettings.archeologist && (
-                <OverlayTrigger overlay={<Tooltip>Reset to last saved value</Tooltip>}>
-                  <button
-                    className="ms-2 p-0 border-0 bg-transparent d-flex align-items-center"
-                    onClick={() => setArcheologistVal(randomAlgoSettings.archeologist)}
-                  >
-                    <span className="visually-hidden">Reset to last saved value</span>
-                    <ArrowClockwise/>
-                  </button>
-                </OverlayTrigger>
-              )}
-            </Form.Label>
-            <div className="d-flex align-items-center gap-4">
-              <Form.Range
-                id="archeologistVal"
-                aria-describedby="archeologistValCounter"
-                min="0"
-                max="10"
-                value={archeologistVal}
-                onChange={e => setArcheologistVal(parseInt(e.currentTarget.value))}
-              />
-              <span id="archeologistValCounter">{archeologistVal}</span>
-            </div>
-          </div>
-          <div className="mb-2">
-            <Form.Label htmlFor="supporterVal" className="d-flex align-items-center gap-2">
-              Supporter
-              <OverlayTrigger
-                overlay={
-                  <Tooltip>
-                    Screenshots from Hall of Fame's members that assist the platform's development (e.g.
-                    donations, being a positive contributor to the platform).
-                  </Tooltip>
-                }
-              >
-                <InfoCircle/>
-              </OverlayTrigger>
-              {supporterVal !== randomAlgoSettings.supporter && (
-                <OverlayTrigger overlay={<Tooltip>Reset to last saved value</Tooltip>}>
-                  <button
-                    className="ms-2 p-0 border-0 bg-transparent d-flex align-items-center"
-                    onClick={() => setSupporterVal(randomAlgoSettings.supporter)}
-                  >
-                    <span className="visually-hidden">Reset to last saved value</span>
-                    <ArrowClockwise/>
-                  </button>
-                </OverlayTrigger>
-              )}
-            </Form.Label>
-            <div className="d-flex align-items-center gap-4">
-              <Form.Range
-                id="supporterVal"
-                aria-describedby="supporterValCounter"
-                min="0"
-                max="10"
-                value={supporterVal}
-                onChange={e => setSupporterVal(parseInt(e.currentTarget.value))}
-              />
-              <span id="supporterValCounter">{supporterVal}</span>
-            </div>
-          </div>
-          <div className="mb-2">
-            <Form.Label htmlFor="viewMaxAgeVal" className="d-flex">
-              Minimum days until a screenshot can be shown again
-              {viewMaxAgeVal !== randomAlgoSettings.viewMaxAge && (
-                <OverlayTrigger overlay={<Tooltip>Reset to last saved value</Tooltip>}>
-                  <button
-                    className="ms-2 p-0 border-0 bg-transparent d-inline-flex align-items-center"
-                    onClick={() => setViewMaxAgeVal(randomAlgoSettings.viewMaxAge)}
-                  >
-                    <span className="visually-hidden">Reset to last saved value</span>
-                    <ArrowClockwise/>
-                  </button>
-                </OverlayTrigger>
-              )}
-            </Form.Label>
-            <div className="d-flex align-items-center gap-4">
-              <Form.Range
-                id="viewMaxAgeVal"
-                aria-describedby="viewMaxAgeValCounter"
-                min="0"
-                max="365"
-                value={viewMaxAgeVal}
-                onChange={e => setViewMaxAgeVal(parseInt(e.currentTarget.value))}
-              />
-              <span id="viewMaxAgeValCounter">{viewMaxAgeVal}</span>
-            </div>
-          </div>
+          <RangeSetting
+            name="Random"
+            value={randomVal}
+            ogValue={randomAlgoSettings.random}
+            min={0}
+            max={10}
+            tooltipHint="Screenshots chosen completely by random."
+            setValue={setRandomVal}
+            inputId="randomVal"
+          />
+          <RangeSetting
+            name="Popular"
+            value={popularVal}
+            ogValue={randomAlgoSettings.popular}
+            min={0}
+            max={10}
+            tooltipHint="Screenshots that have an above average likes/views ratio."
+            setValue={setPopularVal}
+            inputId="popularVal"
+          />
+          <RangeSetting
+            name="Trending"
+            value={trendingVal}
+            ogValue={randomAlgoSettings.trending}
+            min={0}
+            max={10}
+            tooltipHint="Popular screenshots that have the highest likes/views ratio, regardless of age."
+            setValue={setTrendingVal}
+            inputId="trendingVal"
+          />
+          <RangeSetting
+            name="Recent"
+            value={recentVal}
+            ogValue={randomAlgoSettings.recent}
+            min={0}
+            max={10}
+            tooltipHint="Recently posted screenshots (up to a few days old)"
+            setValue={setRecentVal}
+            inputId="recentVal"
+          />
+          <RangeSetting
+            name="Forgotten Ones"
+            value={archeologistVal}
+            ogValue={randomAlgoSettings.archeologist}
+            min={0}
+            max={10}
+            tooltipHint="Screenshots that have not seen any activity for some time."
+            setValue={setArcheologistVal}
+            inputId="archeologistVal"
+          />
+          <RangeSetting
+            name="Supporter"
+            value={supporterVal}
+            ogValue={randomAlgoSettings.supporter}
+            min={0}
+            max={10}
+            tooltipHint="Screenshots from Hall of Fame's members that assist the platform's development (e.g.
+                    donations, being a positive contributor to the platform)."
+            setValue={setSupporterVal}
+            inputId="supporterVal"
+          />
+          <RangeSetting
+            name="Minimum days until a screenshot can be shown again"
+            value={viewMaxAgeVal}
+            ogValue={randomAlgoSettings.viewMaxAge}
+            min={0}
+            max={365}
+            setValue={setViewMaxAgeVal}
+            inputId="viewMaxAgeVal"
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
