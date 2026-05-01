e80550c1437bbffee99b9bc6b1bc47e5c71d1262
cloudgal42 <alternativeego666@gmail.com>
2026-05-01 20:01:38 +0700

    refactor: extract most of grouped cities settings logic to useGroupedCitiesSettings; remove useCreatorInitialGroupedSettings for the new hook

--- src/components/settings/GroupedCitiesSection/GroupedCitiesSection.tsx
+++ src/components/settings/GroupedCitiesSection/GroupedCitiesSection.tsx
@@ -2,7 +2,6 @@ import { Accordion, Alert, Form, Spinner } from "react-bootstrap";
 import { GroupedCityRow } from "./GroupedCityRow.tsx";
 import { AddGroup } from "./AddGroup.tsx";
 import { Fragment, useState } from "react";
-import type { GroupedCityRowSetting } from "../../../interfaces/GroupedCityRowSetting.ts";
 import {
   DragDropProvider,
   type DragEndEvent,
@@ -11,7 +10,7 @@ import {
 import { ErrorScreen } from "../../misc/ErrorScreen/ErrorScreen.tsx";
 import { useLocalStorage } from "usehooks-ts";
 import { GroupedSettingsContext } from "../../../context/GroupedSettingsContext.ts";
-import { useCreatorInitialGroupedSettings } from "../../../hooks/useCreatorInitialGroupedSettings.ts";
+import { useGroupedCitiesSettings } from "../../../hooks/useGroupedCitiesSettings.ts";
 
 import type { GroupCandidate } from "../../../interfaces/GroupCandidate.ts";
 import { ErrorNotification } from "../../misc/LeftNotification/ErrorNotification.tsx";
@@ -26,13 +25,6 @@ export const GroupedCitiesSection = () => {
     "ROW_ALREADY_EXISTS" | "NAME_ALREADY_EXISTS" | ""
   >("");
 
-  const [groupedCitiesOverride, setGroupedCitiesOverride] = useLocalStorage<
-    [string, GroupedCityRowSetting[]][]
-  >(
-    "groupedCitiesOverride",
-    [],
-  );
-
   const [groupedCitiesGenSettings, setGroupedCitiesGenSettings] =
     useLocalStorage<GroupedCityGenSettings>(
       "groupedCitiesGenSettings",
@@ -42,201 +34,24 @@ export const GroupedCitiesSection = () => {
       },
     );
 
-  const groupedCitiesRows = new Map<string, GroupedCityRowSetting[]>(
-    groupedCitiesOverride,
-  );
+  const {
+    groupedCitiesRows,
+    error,
+    isFetching,
+    getOriginFromGroupCandidateId,
+    addGroupCandidate,
+    changeGroupCandidateName,
+    removeGroupCandidate,
+    addGroupedCityEntry,
+    changeGroupedEntryName,
+    removeGroupedEntry,
+    isRowAlreadyExists,
+    handleChangeSettings,
+    isGroupCandidateAlreadyExists,
+  } = useGroupedCitiesSettings(creator);
 
   const [currDraggedGroupedCandidate, setCurrDraggedGroupedCandidate] =
     useState<GroupCandidate | null>(null);
-
-  function isRowAlreadyExists(name: string) {
-    const creatorEntries = groupedCitiesRows.get(creator);
-
-    if (!creatorEntries) return false;
-
-    return creatorEntries.some((entry) => entry.groupedCityName === name);
-  }
-
-  // Fetches data than check if data needs to be created or updated.
-  // Than updates the state via a cb
-  const { isFetching, error } = useCreatorInitialGroupedSettings(
-    creator,
-    (newVal: GroupedCityRowSetting[]) => {
-      const copy = structuredClone(groupedCitiesRows);
-      let valToSet: GroupedCityRowSetting[] | null = null;
-      // We don't want to reset to default if there is already data in local storage
-      if (groupedCitiesRows.has(creator)) {
-        // 1. Get the current list of city names
-        const existingData = groupedCitiesRows.get(creator);
-        const existingCityNames = existingData?.map((cityName) =>
-          cityName.groupedCityName
-        );
-
-        // 2. Filter for new city names
-        const newEntries = newVal.filter((cityName) => {
-          if (!existingCityNames) return false;
-          return !existingCityNames.includes(cityName.groupedCityName);
-        });
-
-        // 2.2. Filter for deleted groups
-        // const deletedGroups = existingData?.filter(cityName =>
-        //   !newVal.includes(cityName)
-        // );
-
-        // 2.3. Filter for deleted group candidates
-
-        // 3. If there are new city names, Update with new city names
-        if (newEntries.length > 0 && existingData) {
-          valToSet = [...existingData, ...newEntries];
-        } else if (existingData) {
-          valToSet = [...existingData];
-        }
-      } else {
-        valToSet = [...newVal];
-      }
-
-      if (valToSet) {
-        console.log("Sorting");
-        valToSet = valToSet.sort((a, b) =>
-          b.groupCandidates.length - a.groupCandidates.length
-        );
-        copy.set(creator, valToSet);
-      }
-      setGroupedCitiesOverride(Array.from(copy.entries()));
-    },
-  );
-
-  function handleChangeSettings(newMap: Map<string, GroupedCityRowSetting[]>) {
-    setGroupedCitiesOverride(Array.from(newMap.entries()));
-  }
-
-  function addGroupedCityEntry(newPosition: "top" | "bottom") {
-    const copy = structuredClone(groupedCitiesRows);
-
-    if (copy && creator) {
-      const creatorSettings = copy.get(creator);
-
-      if (creatorSettings && newPosition === "top") {
-        creatorSettings.unshift({
-          groupCandidates: [{
-            name: `GroupedCity${creatorSettings.length + 1}-1`,
-            isEditable: true,
-            id: crypto.randomUUID(),
-          }],
-          groupedCityName: `GroupedCity${creatorSettings.length + 1}`,
-          isUserCreated: true,
-          id: crypto.randomUUID(),
-        });
-      } else if (creatorSettings) {
-        creatorSettings.push({
-          groupCandidates: [{
-            name: `GroupedCity${creatorSettings.length + 1}-1`,
-            isEditable: true,
-            id: crypto.randomUUID(),
-          }],
-          groupedCityName: `GroupedCity${creatorSettings.length + 1}`,
-          isUserCreated: true,
-          id: crypto.randomUUID(),
-        });
-      }
-    }
-
-    handleChangeSettings(copy);
-  }
-
-  function addGroupCandidate(groupedCityName: string) {
-    const copy = structuredClone(groupedCitiesRows);
-
-    const objToModify = copy.get(creator)?.find((city) =>
-      city.groupedCityName === groupedCityName
-    );
-
-    if (objToModify) {
-      objToModify.groupCandidates = [
-        {
-          name: `${objToModify.groupedCityName}-${
-            objToModify.groupCandidates.length + 1
-          }`,
-          isEditable: true,
-          id: crypto.randomUUID(),
-        },
-        ...objToModify.groupCandidates,
-      ];
-    }
-
-    handleChangeSettings(copy);
-  }
-
-  function removeGroupCandidate(
-    ownerId: string,
-    id: string,
-  ) {
-    const copy = structuredClone(groupedCitiesRows);
-
-    const objToModify = copy.get(creator)?.find((city) => city.id === ownerId);
-
-    if (objToModify) {
-      objToModify.groupCandidates = objToModify.groupCandidates.filter(
-        (entry) => {
-          return entry.id !== id;
-        },
-      );
-    }
-
-    handleChangeSettings(copy);
-  }
-
-  function removeGroupedEntry(id: string) {
-    const copy = structuredClone(groupedCitiesRows);
-
-    let arrToModify = copy.get(creator);
-
-    if (arrToModify) {
-      arrToModify = arrToModify.filter((entry) => entry.id !== id);
-      copy.set(creator, arrToModify);
-    }
-
-    handleChangeSettings(copy);
-  }
-
-  function changeGroupCandidateName(
-    ownerId: string,
-    groupCandidateId: string,
-    newValue: string,
-  ) {
-    const copy = structuredClone(groupedCitiesRows);
-    const objToModify = copy
-      .get(creator)
-      ?.find((city) => city.id === ownerId)
-      ?.groupCandidates.find((city) => city.id === groupCandidateId);
-
-    if (objToModify) {
-      objToModify.name = newValue;
-    }
-
-    handleChangeSettings(copy);
-  }
-
-  function changeGroupedEntryName(id: string, newValue: string) {
-    const copy = structuredClone(groupedCitiesRows);
-
-    const objToModify = copy.get(creator)?.find((city) => city.id === id);
-
-    if (objToModify) {
-      objToModify.groupedCityName = newValue;
-    }
-
-    handleChangeSettings(copy);
-  }
-
-  function getOriginFromGroupCandidateId(
-    copy: Map<string, GroupedCityRowSetting[]>,
-    id: string,
-  ) {
-    return copy.get(creator)?.find((entry) => {
-      return entry.groupCandidates.some((cityName) => cityName.id === id);
-    });
-  }
 
   function handleDragStart(e: DragStartEvent) {
     const sourceId = e.operation.source?.id;
@@ -246,9 +61,7 @@ export const GroupedCitiesSection = () => {
     );
 
     if (origin) {
-      const obj = origin.groupCandidates.find((entry) =>
-        entry.id === sourceId
-      );
+      const obj = origin.groupCandidates.find((entry) => entry.id === sourceId);
       if (obj) setCurrDraggedGroupedCandidate(obj);
     }
     setSettingsError("");
@@ -322,48 +135,6 @@ export const GroupedCitiesSection = () => {
     setCurrDraggedGroupedCandidate(null);
   }
 
-  function isGroupCandidateAlreadyExists(
-    parentId: string,
-    id: string,
-    name: string,
-  ) {
-    // 1. Get all creator entries
-    const creatorEntries = groupedCitiesRows.get(creator);
-
-    if (!creatorEntries) return false;
-
-    // 2. Map all of them to the following data struct:
-    // {
-    //   groupCandidate: string
-    //   parentId: string
-    //   id: string
-    // }
-    const processed = creatorEntries.flatMap((entry) => {
-      return entry.groupCandidates.map((groupedCandidates) => ({
-        groupCandidate: groupedCandidates.name,
-        parentId: entry.id,
-        id: groupedCandidates.id,
-      }));
-    });
-
-    console.log(processed);
-
-    // 3. Check if:
-    // groupCandidate exists in whole list. If yes, return entries that match this
-    // AND if those returned results have different ID compared to id
-    // OR if those returned results have a diff parent ID
-    const duplicatedNameEntries = processed
-      .filter((nameEntries) => nameEntries.groupCandidate === name);
-
-    if (duplicatedNameEntries.length < 1) return false;
-
-    return duplicatedNameEntries
-      .some((nameEntries) =>
-        nameEntries.id !== id ||
-        nameEntries.parentId !== parentId
-      );
-  }
-
   const creatorEntries = groupedCitiesRows && groupedCitiesRows.get(creator);
 
   const creatorEntriesWithGroupCandidates = creatorEntries &&
@@ -414,7 +185,7 @@ export const GroupedCitiesSection = () => {
             position="top"
           />
           <div className="mt-3">
-            {creatorEntriesWithGroupCandidates.map((groupedCitiesRow, i) => (
+            {creatorEntriesWithGroupCandidates.map((groupedCitiesRow) => (
               <GroupedCityRow key={groupedCitiesRow.id} {...groupedCitiesRow} />
             ))}
             {creatorEntriesWithoutGroupCandidates.length > 0 && (
@@ -425,10 +196,7 @@ export const GroupedCitiesSection = () => {
                     ({creatorEntriesWithoutGroupCandidates.length} result(s))
                   </Accordion.Header>
                   <Accordion.Body>
-                    {creatorEntriesWithoutGroupCandidates.map((
-                      groupedCitiesRow,
-                      i,
-                    ) => (
+                    {creatorEntriesWithoutGroupCandidates.map((groupedCitiesRow) => (
                       <Fragment key={groupedCitiesRow.id}>
                         <GroupedCityRow {...groupedCitiesRow} />
                       </Fragment>
